06af0ae1b521a682b40a8e58527dbbb714b33315
statkclee <kwangchun.lee.7@gmail.com>
2015-09-14 00:12:50 +0900

    move SoftLayer to AWS

---
+++ xwmooc.org/util/json_api.php
@@ -0,0 +1,4 @@
+<br />
+<b>Warning</b>:  require_once(db.php) [<a href='function.require-once'>function.require-once</a>]: failed to open stream: No such file or directory in <b>/homepages/17/d88943663/htdocs/cloudsocial/api/dev/basiclti/orm.php</b> on line <b>3</b><br />
+<br />
+<b>Fatal error</b>:  require_once() [<a href='function.require'>function.require</a>]: Failed opening required 'db.php' (include_path='.:/usr/lib/php5') in <b>/homepages/17/d88943663/htdocs/cloudsocial/api/dev/basiclti/orm.php</b> on line <b>3</b><br />
